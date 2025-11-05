5bbefb839d50344ef0df77e1f2a9401b52fc7758
Grayson <mcclellangg@gmail.com>
2025-11-05 10:34:22 -0500

    add justfile; solve day04pt1;

--- 2015/day_04/aoc_2015_04.py
+++ 2015/day_04/aoc_2015_04.py
@@ -1,4 +1,10 @@
+"""
+url: https://adventofcode.com/2015/day/4
+tags: md5, hash, cryptography
+"""
+
 from pathlib import Path
+import hashlib
 
 
 # Load input
@@ -8,17 +14,38 @@ def parse_input() -> list:
     return input_file.read_text().strip()
 
 
-def part_1(data: list) -> int:
-    """Return solution for part_1."""
-    return 0
+def part_1(data: list) -> str:
+    """Brute force solution to get lowest decimal number that creates the valid MD5 hash.
+
+    Will there always be an answer?
+
+    Is there a way to apply a sorting algo to decimal generation? How do You tell if a guess is better or worse?
+    """
+    k_prefix = "".join(data)
+
+    # Generate decimals
+    for i in range(0, 1000000):
+        k_suffix = i
+
+        k = f"{k_prefix}{k_suffix}"
+        hex_hash = hashlib.md5(k.encode()).hexdigest()
+
+        if hex_hash[:5] == "00000":
+            print(f"Suffix found: {k_suffix}")
+            print(f"Match found: {hex_hash}")
+            return k_suffix
+
+    return "No hash found..."
 
 
 def part_2(data: list) -> int:
-    """Return solution for part_2."""
+    """Return solution for part_2.
+    url: https://adventofcode.com/2015/day/4#part2
+    """
     return 0
 
 
-if __name__ == "__main___":
+if __name__ == "__main__":
     input_data = parse_input()
     print(f"PT_1| Solution: {part_1(input_data)}")
-    print(f"PT_2| Solution: {part_2(input_data)}")
+    # print(f"PT_2| Solution: {part_2(input_data)}")
---
+++ justfile
@@ -0,0 +1,6 @@
+# use PowerShell instead of sh:
+set shell := ["powershell.exe", "-c"]
+
+# Run black on all practice problems: venv must be activated!
+format:
+    black .\2015\*
