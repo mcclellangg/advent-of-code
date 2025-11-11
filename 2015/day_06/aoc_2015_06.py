067aeaaec23a1063984a59c3aa49cecfa712c1c1
Grayson <mcclellangg@gmail.com>
2025-11-11 10:01:10 -0500

    solve day6:pt1;

--- 2015/day_06/aoc_2015_06.py
+++ 2015/day_06/aoc_2015_06.py
@@ -1,6 +1,6 @@
 """
 url:    https://adventofcode.com/2015/day/6
-tags:   9:48 -> 9:59 | 10:10 -> 10:33
+tags:
 """
 
 from pathlib import Path
@@ -52,13 +52,38 @@ ACTION_TO_VAL = {"on": 1, "off": 0, "toggle": {0: 1, 1: 0}}
 
 
 def part_1(data: list) -> int:
+    """
+    Brute force solution.
+    """
     assert len(data) == 300
+    # {"on": [(000,000), (759,859)]}
 
     # iterate instructions
-    # update grid based on action
-    # return count of 1s in grid
+    for i in data:
+        action = list(i)[0]
+        val = ACTION_TO_VAL[action]
+        coords = i[action]
+        start_r, start_c = coords[0][0], coords[0][1]
+        end_r, end_c = coords[1][0], coords[1][1]
 
-    return 0
+        # Update the matrix
+        if action == "toggle":
+            for r in range(start_r, end_r + 1):
+                for c in range(start_c, end_c + 1):
+                    light_grid[r][c] = ACTION_TO_VAL["toggle"][light_grid[r][c]]
+
+        else:
+            for r in range(start_r, end_r + 1):
+                for c in range(start_c, end_c + 1):
+                    light_grid[r][c] = val
+
+    # Count lights that are on
+    ttl_lights_on = 0
+
+    for row in range(len(light_grid)):
+        ttl_lights_on += light_grid[row].count(1)
+
+    return ttl_lights_on
 
 
 def part_2(data: list) -> int:
