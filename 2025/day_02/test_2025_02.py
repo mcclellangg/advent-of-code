31dd1d95f85ea72490b301db11f7fad50fd286a8
grayson <grayson@vera.mynetworksettings.com>
2025-12-03 10:41:42 -0500

    update parse_input to properly handle product_data;

--- 2025/day_02/test_2025_02.py
+++ 2025/day_02/test_2025_02.py
@@ -11,14 +11,14 @@ def test_part_1_sample_input():
 
 def test_part_1_full_input():
     result = challenge.part_1(full_input)
-    assert result == 0
+    assert result == 18952700150
 
 
-def test_part_2_sample_input():
-    result = challenge.part_2(sample_input)
-    assert result == 0
+# def test_part_2_sample_input():
+#     result = challenge.part_2(sample_input)
+#     assert result == 0
 
 
-def test_part_2_full_input():
-    result = challenge.part_2(full_input)
-    assert result == 0
+# def test_part_2_full_input():
+#     result = challenge.part_2(full_input)
+#     assert result == 0
